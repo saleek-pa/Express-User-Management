3c0af0a4fb6e6cff07814bd63ef1dc448f90a1bd
saleek <saleek.pa@gmail.com>
2023-10-14 23:57:41 +0530

    Connected MongoDB

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express()
+
+mongoose.connect("mongodb://127.0.0.1:27017/example");
+
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error: "));
+// db.once("open", function () {
+//     console.log("Connected successfully");
+// });
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    age: Number
+})
+
+const userModel = new mongoose.model("User", userSchema)
 
 const userRouter = require('./routes/userRoutes')
 
